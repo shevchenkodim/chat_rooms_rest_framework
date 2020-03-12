9adb55e595fae7e7ce5ec941d98a2480ef9151e0
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-12 22:46:20 +0200

    djoser & get, post dialog chat
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- chat_room/serializers.py
+++ chat_room/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from chat_room.models import Room
+from chat_room.models import Room, Chat
 
 class UserSerializer(serializers.ModelSerializer):
     """Сериализация пользователя"""
@@ -16,3 +16,18 @@ class RoomSerializers(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ("creater", "invited", "date")
+
+
+class ChatSerializers(serializers.ModelSerializer):
+    """Сериализация чата"""
+    user = UserSerializer()
+    class Meta:
+        model = Chat
+        fields = ("user", "text", "date")
+
+
+class ChatPostSerializers(serializers.ModelSerializer):
+    """Сериализация чата"""
+    class Meta:
+        model = Chat
+        fields = ("room", "text")
--- chat_room/urls.py
+++ chat_room/urls.py
@@ -3,4 +3,5 @@ from chat_room.views import *
 
 urlpatterns = [
     path('room/', Rooms.as_view()),
+    path('dialog/', Dialog.as_view()),
 ]
