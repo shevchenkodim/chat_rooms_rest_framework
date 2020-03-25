ecd72660318d1d58dc0975ff95952dacaf35788c
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-25 23:02:58 +0200

    Token, list of rooms, exit implementation
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- chat-vue/src/components/Home.vue
+++ chat-vue/src/components/Home.vue
@@ -1,16 +1,34 @@
 <template>
     <div>
         <h1>Чат на vue js</h1>
-        <button @click="goLogin">Вход</button>
+        <button v-if="!auth" @click="goLogin">Вход</button>
+        <button v-else @click="logout">Выход</button>
+        <Room v-if="auth"></Room>
     </div>
 </template>
 
 <script>
+    import Room from '@/components/Room'
+
     export default{
         name: "Home",
+        components: {
+            Room
+        },
+        computed: {
+            auth() {
+                if (sessionStorage.getItem("auth_token")) {
+                    return true
+                }
+            }
+        },
         methods: {
             goLogin() {
                 this.$router.push({name: "login"})
+            },
+            logout() {
+                sessionStorage.removeItem("auth_token")
+                window.location = '/'
             },
         },
     }
